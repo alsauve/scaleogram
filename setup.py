2208b6c89a460359e0080c66f1080f056aa63d02
Alexandre Sauve <asauve@gmail.com>
2019-03-28 01:57:35 +0100

    version bump 0.9.3.3 => fix dependencies on kaggle

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open('requirements.txt') as f:
     
 setuptools.setup(
      name='scaleogram',  
-     version='0.9.3',
+     version='0.9.3.2',
      author="Alexandre Sauve",
      author_email="asauve@gmail.com",
      description="User friendly scaleogram plot for Continuous Wavelet Transform",
