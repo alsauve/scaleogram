bcd53224c4ae8eeb5aec7345673c640e32e3bd55
Alexandre Sauve <asauve@gmail.com>
2019-04-01 22:32:25 +0200

    version bump v0.9.4

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open('requirements.txt') as f:
     
 setuptools.setup(
      name='scaleogram',  
-     version='0.9.3.2',
+     version='0.9.4',
      author="Alexandre Sauve",
      author_email="asauve@gmail.com",
      description="User friendly scaleogram plot for Continuous Wavelet Transform",
